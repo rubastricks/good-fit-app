c1c3091c69924bf82428f0fd6f7192a700e53757
Alisson Rubas <85270792+rubastricks@users.noreply.github.com>
2023-01-19 16:17:52 -0500

    Update SearchExercises.js

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
-import HorizontalScrollbar from "./HorizontalScrollbar";
+
 
 const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
@@ -81,11 +81,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
         </Button>
       </Box>
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
-        {/* <HorizontalScrollbar
-          data={bodyParts}
-          bodyPart={bodyPart}
-          setBodyPart={setBodyPart}
-        /> */}
+       
       </Box>
     </Stack>
   );
